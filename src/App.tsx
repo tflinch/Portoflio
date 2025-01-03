cd9a0594ede56b7f0c98842553a17d4c3ce42496
tflinch <tflinch16@gmail.com>
2025-01-03 12:12:58 -0500

    feat: add About.tsx page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar/Navbar'
 import Home from './pages/Home'
 import Footer from './components/Footer/Footer'
 import Experience from './pages/Experience'
+import About from './pages/About'
 
 function App() {
   const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
@@ -16,6 +17,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home theme={theme} />} />
         <Route path='/experience' element={<Experience theme={theme} />} />
+        <Route path='/about' element={<About theme={theme} />} />
       </Routes>
       <Footer theme={theme} />
     </Router>
---
+++ src/pages/About.tsx
@@ -0,0 +1,154 @@
+import { RiCss3Line, RiGitRepositoryLine, RiHtml5Line, RiJavascriptLine, RiReactjsLine, RiDatabase2Line, RiNodejsLine, RiCloudLine, RiFirebaseLine } from '@remixicon/react';
+
+interface aboutProps {
+    theme: 'light' | 'dark'
+}
+
+const About: React.FC<aboutProps> = ({ theme }) => {
+
+    return (
+        <main className={theme}>
+            <section className="section">
+                <div className="wrapper" data-width="wide">
+                    <div className="equal-columns" data-gap="large" data-alignment="centered">
+                        <div>
+                            <img className='about-image' src="../../src/assets/img/tommy-flinch.jpg" alt="man smiling for a candid photo" />
+                        </div>
+                        <div className="flow font-family-basic">
+                            <h2 className={theme}>Hi!👋🏾 </h2>
+                            <p className="about_content">
+                                I’m Tommy Flinch, a passionate software engineer and creative problem solver based in New York. My work bridges the gap between innovative technology and impactful design, with a focus on creating scalable, user-centric digital solutions. Whether I’m developing back-end systems or crafting seamless user interfaces, I strive to deliver meaningful experiences that leave a lasting impact.                                        </p>
+                            <p className="about_content">
+                                Throughout my career, I’ve had the privilege of contributing to projects that challenge me to combine technical expertise with creative thinking. From leading the development of a fire alert system leveraging geolocation and Leaflet.js, to building dynamic APIs for robotic automation, I’ve consistently embraced opportunities to expand my skill set and deliver solutions that address real-world challenges.                                        </p>
+                            <p className="about_content">
+                                My journey in technology has also been shaped by a love for learning and mentorship. I’ve worked on projects that demanded collaboration across disciplines, balancing the needs of designers, engineers, and stakeholders to stay within scope while ensuring quality. From accessibility testing to unit testing, these experiences taught me the importance of meticulous attention to detail and fostering teamwork to achieve shared goals.
+                            </p>
+                            <p className="about_content">
+                                Beyond coding, I am deeply committed to empowering others through technology. Whether mentoring robotics teams or collaborating with nonprofits to teach full stack engineering education, I strive to inspire the next generation of innovators. Technology has the power to change lives, and I’m dedicated to making it accessible to everyone, one project at a time.                                        </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="section">
+                <div className="wrapper" data-width='narrow'>
+                    <div className="equal-columns" data-alignment="centered" data-auto='none'>
+                        <div className="card">
+                            <p>Years of Work</p>
+                            <p>Completed Projects</p>
+                            <p>Satisfied Customers</p>
+                        </div>
+                        <div className="card">
+                            <div className="skills__box">
+                                <div className="skills__group">
+                                    <div className="skills__data">
+                                        <RiHtml5Line className='my-icon' />
+
+                                        <div>
+                                            <h3 className="skills__name">HTML</h3>
+                                            <span className="skills__level">Advance</span>
+                                        </div>
+                                    </div>
+
+                                    <div className="skills__data">
+                                        <RiCss3Line />
+
+                                        <div>
+                                            <h3 className="skills__name">CSS</h3>
+                                            <span className="skills__level">Advance</span>
+                                        </div>
+                                    </div>
+
+                                    <div className="skills__data">
+                                        <RiJavascriptLine />
+
+                                        <div>
+                                            <h3 className="skills__name">JavaScript</h3>
+                                            <span className="skills__level">Advance</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="skills__group">
+                                    <div className="skills__data">
+                                        <RiReactjsLine />
+
+                                        <div>
+                                            <h3 className="skills__name">React</h3>
+                                            <span className="skills__level">Intermediate</span>
+                                        </div>
+                                    </div>
+
+                                    <div className="skills__data">
+                                        <img className='icon' src={'../../src/assets/icons/python.png'} alt="python logo" />
+
+                                        <div>
+                                            <h3 className="skills__name">Python</h3>
+                                            <span className="skills__level">Intermediate</span>
+                                        </div>
+                                    </div>
+
+                                    <div className="skills__data">
+                                        <RiGitRepositoryLine />
+
+                                        <div>
+                                            <h3 className="skills__name">Git</h3>
+                                            <span className="skills__level">Intermediate</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="skills__group">
+                                    <div className="skills__data">
+                                        <img className='icon' src={'../../src/assets/icons/php-fill.png'} alt="python logo" />
+
+                                        <div>
+                                            <h3 className="skills__name">PHP</h3>
+                                            <span className="skills__level">Basic</span>
+                                        </div>
+                                    </div>
+
+                                    <div className="skills__data">
+                                        <RiDatabase2Line />
+                                        <div>
+                                            <h3 className="skills__name">MySQL</h3>
+                                            <span className="skills__level">Intermediate</span>
+                                        </div>
+                                    </div>
+
+                                    <div className="skills__data">
+                                        <RiFirebaseLine />
+
+                                        <div>
+                                            <h3 className="skills__name">Firebase</h3>
+                                            <span className="skills__level">Intermediate</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="skills__group">
+                                    <div className="skills__data">
+                                        <RiNodejsLine />
+
+                                        <div>
+                                            <h3 className="skills__name">Node Js</h3>
+                                            <span className="skills__level">Advance</span>
+                                        </div>
+                                    </div>
+
+                                    <div className="skills__data">
+                                        <RiCloudLine />
+
+                                        <div>
+                                            <h3 className="skills__name">Cloud</h3>
+                                            <span className="skills__level">Advance</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </section>
+        </main>
+    )
+}
+
+export default About
